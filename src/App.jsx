50d2df35d32e8bd4e2c510b01e0d4c028998fadb
Luiz Meneghim <luiz.meneghim@hotmail.com>
2023-01-09 15:46:25 -0700

    first push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <h1 className="text-center">Hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
