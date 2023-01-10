2e26cff8b662ef88b4089ee34d39038576d3b1f9
Luiz Meneghim <luiz.meneghim@hotmail.com>
2023-01-10 14:43:18 -0700

    adding api integration

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Quiz() {
+  const [quiz, setQuiz] = useState([]);
+
+  useEffect(() => {
+    async function getQuiz() {
+      const res = await fetch('https://opentdb.com/api.php?amount=5');
+      const data = await res.json();
+      setQuiz(data.results);
+    }
+    getQuiz();
+  }, []);
+  console.log(quiz);
+
+  const question = quiz.map((question) => {
+    return (
+      <div className="question">
+        <h2 className="font-bold">
+          Question:
+          {question.question}
+        </h2>
+      </div>
+    );
+  });
+
   return (
     <div>
-      <h2>Here are the quizzes</h2>
+      <h2>Here is your Quiz:</h2>
+      {question}
     </div>
   );
 }
